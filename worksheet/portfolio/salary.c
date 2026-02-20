e9d06854747174bbc2e60155c843853ab28ba887
jwah44 <zxck0708@leeds.ac.uk>
2026-02-20 01:52:08 +0000

    worksheet complete

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -4,22 +4,22 @@
 /*
  * Potrfolio submission
  * Name: Joshua Momcilovic
- * ID: 2018
+ * ID: 201876926
  */
 
 int main( void ) {
 
     // define and initialise variables for the problem data 
-   float salary = 40000;
-   float NI = 0.5;
-   float tax = 0.1;
+   float salary = 36250;
+   float NI = 0.08;
+   float tax = 0.15;
 
    float NIcontrib;
    float taxcontrib;
    float takehome;
     // calculate the deductions and final take-home salary
    NIcontrib = salary*NI;
-   taxcontrib = (salary - NIcontrib)*tax;
+   taxcontrib = (salary - NIcontrib- 12500)*tax;
    takehome = salary - (NIcontrib + taxcontrib);
     // Use only these print statement with appropriate formatting and variable names
    printf("Salary £%.2f\n",salary);
